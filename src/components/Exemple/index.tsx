7c29519a6b920f95e39c9ca64b98f14186c98275
Gabriel <gabrielmonteiroduete@gmail.com>
2023-03-19 00:34:48 -0300

    chore: add exemples page and configure paths imports

--- src/components/Exemple/index.tsx
+++ src/components/Exemple/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import * as S from './styles'
 
-type ExempleTypes = {
+type ExempleProps = {
   pascalCase: string
 }
 
-const Exemple = ({ pascalCase }: ExempleTypes) => {
+const Exemple = ({ pascalCase }: ExempleProps) => {
   return (
     <S.Wrapper data-testid='exemple'>Exemple component {pascalCase}</S.Wrapper>
   )
